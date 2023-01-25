355258b376aa277db5ef87f5d2fc8f6d9a84a399
Joël Galeran <Jolg42@users.noreply.github.com>
2023-01-25 15:29:03 +0100

    small changes, more debug

--- api/index.js
+++ api/index.js
@@ -1,23 +1,32 @@
-const { PrismaClient, Prisma } = require('@prisma/client')
-const client = new PrismaClient()
+const { PrismaClient, Prisma } = require("@prisma/client");
+const client = new PrismaClient();
 
 export default async (req, res) => {
+  console.log("Runtime");
+  console.log(process.platform);
+  console.log(process.release);
+  console.log(process.config);
+  console.log(process.env);
+
   try {
-    await client.user.deleteMany({})
+    await client.user.deleteMany({});
+
+    process.env.DEBUG = "*";
 
     await client.user.create({
       data: {
-        id: '1234567890',
-        email: 'alice@prisma.io',
-        nick: 'al',
-        // name: 'Alice',
+        id: "1234567890",
+        email: "alice@prisma.io",
+        nick: "al",
+        // Comment/uncomment this and in schema.prisma
+        name: "Alice",
       },
-    })  
+    });
   } catch (e) {
-    console.log(e.message)
+    console.log(e.message);
 
-    return res.status(500).json({ error: e.message })
+    return res.status(500).json({ error: e.message });
   }
 
-  return res.status(200).json({ ok: true })
-}
+  return res.status(200).json({ ok: true });
+};
---
+++ build.js
@@ -0,0 +1,7 @@
+// Add `"build": "node build.js"` to package.json scripts first
+
+console.log("Build");
+console.log(process.platform);
+console.log(process.release);
+console.log(process.config);
+console.log(process.env);
